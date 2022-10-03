3baf255fc008a13d38899457be0da33509fb9d77
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-10-03 14:48:55 +0900

    feat : 플레이어는 1 ~ 9까지 서로 다른 임의의 숫자 3자리를 제시 아래와 같은 조건 존재   1. 3자리 숫자 체크   2. 서로 다른 임의의 수 체크   3. 숫자만으로 이뤄진 값 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,30 @@
 package baseball;
 
 import  camp.nextstep.edu.missionutils.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Application {
-
+    public static String question= "숫자를 입력해주세요 : ";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int number = Randoms.pickNumberInRange(100,999);
+        System.out.print(question);
+        String suggest = Console.readLine();
+        if(validation(suggest)) throw new IllegalArgumentException();
     }
 
+    public static boolean validation(String input) {
+        if (!input.matches("-?\\d{3}+")) return false; //숫자만으로 이뤄진 3자리 수 체크
+
+        Set set = new HashSet();
+        String[] numbers = input.split("");
+        for(String number : numbers){ // 같은 숫자 존재 유무 체크
+            if(set.contains(number)) return false;
+            set.add(number);
+        }
+        return true;
+    }
 }
