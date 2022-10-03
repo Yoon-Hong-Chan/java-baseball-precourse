f84d773eff37f79cc68fe177d93a631c8a4b6b06
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-10-03 19:21:51 +0900

    chore : 기존 테스트 코드 유지

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,16 +1,12 @@
 package baseball;
 
-import baseball.service.JudgeBallCount;
-import baseball.service.PlayBaseBall;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.in;
 
 class ApplicationTest extends NsTest {
 
