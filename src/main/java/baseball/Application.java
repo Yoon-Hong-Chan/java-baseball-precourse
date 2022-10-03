470dab4da08ec767035a317dbee140313fee821d
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-10-03 18:49:52 +0900

    refactor : 기존 로직 리팩토링에 따른 메인 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,74 +1,11 @@
 package baseball;
 
-import  camp.nextstep.edu.missionutils.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import baseball.service.PlayBaseBall;
 
 public class Application {
-    public static String question= "숫자를 입력해주세요 : ";
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        while(true)
-            if(playGame().equals("2"))break;
-    }
-
-    public static String playGame(){
-        String number;
-        while(true){
-            String tmpNumber = String.valueOf(Randoms.pickNumberInRange(100,999));
-            if(checkDuplicateNumber(tmpNumber)){
-                number = tmpNumber;
-                break;
-            }
-        }
-        System.out.println(number);
-        while (true){
-            System.out.print(question);
-            String suggest = Console.readLine();
-            if(!inputValidation(suggest)) throw new IllegalArgumentException();
-            if(distinguishBallCount(number,suggest))break;
-        }
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        return Console.readLine();
-    }
-    public static boolean distinguishBallCount(String number, String input){
-        int strike = 0;
-        int ball = 0;
-        List<String> numbers = Arrays.asList(number.split(""));
-        List<String> inputs = Arrays.asList(input.split(""));
-        for(int i = 0; i < numbers.size(); i++){
-            if(numbers.get(i).equals(inputs.get(i))) {
-                strike++;
-                continue;
-            }
-            if(numbers.contains(inputs.get(i))) ball++;
-        }
-        StringBuilder stringBuilder = new StringBuilder();
-        if(ball != 0) stringBuilder.append(ball+"볼 ");
-        if(strike != 0) stringBuilder.append(strike+"스트라이크");
-        if(ball == 0 && strike ==0 ) stringBuilder.append("낫싱");
-        System.out.println(stringBuilder);
-
-        return (strike == 3);
-    }
-
-    public static boolean checkDuplicateNumber(String input){
-        Set set = new HashSet();
-        String[] numbers = input.split("");
-        for(String number : numbers){ // 같은 숫자 존재 유무 체크
-            if(set.contains(number)) return false;
-            set.add(number);
-        }
-        return true;
-    }
-
-    public static boolean inputValidation(String input) {
-        if (!input.matches("-?\\d{3}+")) return false; //숫자만으로 이뤄진 3자리 수 체크
-        return checkDuplicateNumber(input); // 중복 체크
+        PlayBaseBall playBaseBall = new PlayBaseBall();
+        playBaseBall.startGame();
     }
 }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,17 +1,19 @@
 package baseball;
 
+import baseball.service.JudgeBallCount;
+import baseball.service.PlayBaseBall;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.in;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -36,36 +38,4 @@ class ApplicationTest extends NsTest {
         Application.main(new String[]{});
     }
 
-    @Test
-    void 입력값유효검사(){
-        //given
-        String input = "123";
-        String abnormalCase = "12345";
-        String abnormalCase2 = "12s";
-        String abnormalCase3 = "111";
-
-        //when
-        //then
-        assertThat(Application.inputValidation(input)).isTrue();
-        assertThat(Application.inputValidation(abnormalCase)).isFalse();
-        assertThat(Application.inputValidation(abnormalCase2)).isFalse();
-        assertThat(Application.inputValidation(abnormalCase3)).isFalse();
-    }
-
-    @Test
-    void 볼판별테스트(){
-        String computerInput = "123";
-        String strike3 = "123";
-        String ball3 = "312";
-        String ball2strike1 = "321";
-        String nothing = "456";
-
-        //when
-        //then
-        assertThat(Application.distinguishBallCount(computerInput,strike3)).isTrue();
-        assertThat(Application.distinguishBallCount(computerInput,ball3)).isFalse();
-        assertThat(Application.distinguishBallCount(computerInput,ball2strike1)).isFalse();
-        assertThat(Application.distinguishBallCount(computerInput,nothing)).isFalse();
-
-    }
 }
