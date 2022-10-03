ac15f2a25a811c8ec2a541825796c118483f779d
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-10-03 15:21:27 +0900

    feat : 볼, 스트라이크, 낫싱의 결과 판별 및 출력
      1. 같은 수가 같은 자리에 있으면 스트라이크
      2. 다른 자리에 있으면 볼
      3. 같은 수가 전혀 없으면 낫싱

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import  camp.nextstep.edu.missionutils.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,13 +11,35 @@ public class Application {
     public static String question= "숫자를 입력해주세요 : ";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int number = Randoms.pickNumberInRange(100,999);
+        String number = String.valueOf(Randoms.pickNumberInRange(100,999));
         System.out.print(question);
         String suggest = Console.readLine();
-        if(validation(suggest)) throw new IllegalArgumentException();
+        if(inputValidation(suggest)) throw new IllegalArgumentException();
+        distinguishBallCount(number,suggest);
     }
 
-    public static boolean validation(String input) {
+    public static boolean distinguishBallCount(String number, String input){
+        int strike = 0;
+        int ball = 0;
+        List<String> numbers = Arrays.asList(number.split(""));
+        List<String> inputs = Arrays.asList(input.split(""));
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i).equals(inputs.get(i))) {
+                strike++;
+                continue;
+            }
+            if(numbers.contains(inputs.get(i))) ball++;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        if(ball != 0) stringBuilder.append(ball+"볼 ");
+        if(strike != 0) stringBuilder.append(strike+"스트라이크");
+        if(ball == 0 && strike ==0 ) stringBuilder.append("낫싱");
+        System.out.println(stringBuilder);
+
+        return (strike == 3);
+    }
+
+    public static boolean inputValidation(String input) {
         if (!input.matches("-?\\d{3}+")) return false; //숫자만으로 이뤄진 3자리 수 체크
 
         Set set = new HashSet();
